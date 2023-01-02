c5df4580dadb906e6be2e4f39ab543cd8d3a9eec
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-02 17:04:25 +0600

    This is the hover

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,30 +1,55 @@
 import React from 'react';
 
 const Services = () => {
+
+    const services = [
+        {
+            name: 'Medical',
+            description: 'The Doctor of Medicine (M.D.) degree is a five-year graduate program intended to teach students the essentials of being a Medical Doctor..',
+            img: 'img'
+        },
+        {
+            name: 'Consultant',
+            description: 'A consultant physician is a senior doctor who practises in one of the medical specialties. Once specialty training has been completed.',
+            img: 'img'
+        },
+        {
+            name: 'Physiologist',
+            description: 'Physiologists are highly trained medical scientists with a master degree or doctoral degree.With zeal and expertise in studying how humans',
+            img: 'img'
+        },
+        {
+            name: 'Physiologist', description: 'Physiologists are highly trained medical scientists with a master degree or doctoral degree.With zeal and expertise in studying how humans',
+            img: 'img'
+        },
+        {
+            name: 'Physiologist', description: 'Physiologists are highly trained medical scientists with a master degree or doctoral degree.With zeal and expertise in studying how humans',
+            img: 'img'
+        },
+    ]
     return (
         <div>
             <div>
                 <h1 className='text-center text-indigo-500 font-sans'>OUR SERVICES</h1>
                 <h1 className='text-3xl text-center text-indigo-500 font-bold mb-3'>
                     Services We Provide.</h1>
-
             </div>
 
-            <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 justify-items-center'>
-
-
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <figure className="px-10 pt-10">
-                        <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
-                    </figure>
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                        <div className="card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 justify-items-center gap-3 px-5'>
+                {
+                    services.map((service) =>
+                        <div className='grid justify-items-start  hover:scale-125 '>
+                            <figure className="px-10 pt-10">
+                                <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                            </figure> 
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{service.name}</h2>
+                                <p>{service.description}</p>
+                            </div>
                         </div>
-                    </div>
-                </div>
+
+                    )
+                }
 
 
             </div>
