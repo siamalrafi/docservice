f778a2c78e42d6ec73f8b13f41d9aeec7f49558a
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-02 16:39:12 +0600

    This is the next

--- src/Pages/Home/Treatment/Treatment.jsx
+++ src/Pages/Home/Treatment/Treatment.jsx
@@ -23,38 +23,41 @@ const Treatment = () => {
 
                     <div tabIndex={0} className="collapse my-5 collapse-plus border border-indigo-500 rounded-box">
                         <div className="collapse-title text-xl font-medium">
-                            Focus me to see content
+                            What do we treat?
                         </div>
                         <div className="collapse-content">
                             <p tabIndex={0}>
-                                attribute is necessary to make the div focusable</p>
+                                Common health issue Ask anything you would normally ask your GP. You can have an instant video with one of our GPs via a digital consultation from anywhere, at any time of day. Specialist advice Sometimes it's helpful to see a specialist when you want. And get educational advice, pre-screening, monitoring and follow-ups.</p>
                         </div>
                     </div>
+
                     <div tabIndex={1} className="collapse collapse-plus border border-indigo-500 rounded-box">
                         <div className="collapse-title text-xl font-medium">
-                            Focus me to see content
+                            How does it work?
                         </div>
                         <div className="collapse-content">
                             <p tabIndex={1}>
-                                attribute is necessary to make the div focusable</p>
+                                You can register on our App and access several medical services easily. The basic video consultation service is straightforward: search for your relevant doctor with easy filters, talk to your doctor directly as included in your DocTime healthcare package or pay just prior the on-demand consultation starts. Then access your prescription uploaded by the doctor and you can order medicine or tests as recommended</p>
                         </div>
                     </div>
+
                     <div tabIndex={2} className="collapse my-5 collapse-plus border border-indigo-500 rounded-box">
                         <div className="collapse-title text-xl font-medium">
-                            Focus me to see content
+                            Who provides consultation on DocService?
                         </div>
                         <div className="collapse-content">
                             <p tabIndex={2}>
-                                attribute is necessary to make the div focusable</p>
+                                Only certified MBBS doctors can provide consultation via the DocTime app.
+                            </p>
                         </div>
                     </div>
                     <div tabIndex={3} className="collapse my-5 collapse-plus border border-indigo-500 rounded-box">
                         <div className="collapse-title text-xl font-medium">
-                            Focus me to see content
+                            When are the doctors available for consultation?
                         </div>
                         <div className="collapse-content">
                             <p tabIndex={3}>
-                                attribute is necessary to make the div focusable</p>
+                                Doctors are available according to their time availability. Our platform is available 24/7. Any doctor can come online at any time if they want.</p>
                         </div>
                     </div>
 
