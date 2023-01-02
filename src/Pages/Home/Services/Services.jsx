3fc7c1d26487cec13dfb0ca8da28391dbefb66d5
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-02 17:22:22 +0600

    This is the next

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import Medical from '../../../assets/skills/Medical.png';
+import Consultant from '../../../assets/skills/Consultant.png';
+import Physiologist from '../../../assets/skills/Physiologist.png';
+import img1 from '../../../assets/skills/img1.png';
+import img2 from '../../../assets/skills/img2.png';
+import img3 from '../../../assets/skills/img3.png';
+
 
 const Services = () => {
 
@@ -6,27 +13,33 @@ const Services = () => {
         {
             name: 'Medical',
             description: 'The Doctor of Medicine (M.D.) degree is a five-year graduate program intended to teach students the essentials of being a Medical Doctor..',
-            img: 'img'
+            img: Medical
         },
         {
             name: 'Consultant',
             description: 'A consultant physician is a senior doctor who practises in one of the medical specialties. Once specialty training has been completed.',
-            img: 'img'
+            img: Consultant
         },
         {
             name: 'Physiologist',
             description: 'Physiologists are highly trained medical scientists with a master degree or doctoral degree.With zeal and expertise in studying how humans',
-            img: 'img'
+            img: Physiologist
         },
         {
-            name: 'Physiologist', description: 'Physiologists are highly trained medical scientists with a master degree or doctoral degree.With zeal and expertise in studying how humans',
-            img: 'img'
+            name: 'Surgical ', description: 'The surgeon is responsible for the preoperative diagnosis of the patient, for performing the operation, and for providing the patient with postoperative surgical care and treatment',
+            img: img1
         },
         {
-            name: 'Physiologist', description: 'Physiologists are highly trained medical scientists with a master degree or doctoral degree.With zeal and expertise in studying how humans',
-            img: 'img'
+            name: 'Therapist', description: 'A therapist is a licensed counselor or psychologist who can use talk therapy to help you treat mental health symptoms and improve how you manage stress and relationships.',
+            img: img2
         },
-    ]
+        {
+            name: 'Psychiatrist ', description: 'A psychiatrist is a physician who specializes in psychiatry, the branch of medicine devoted to the diagnosis, prevention, study, and treatment of mental disorders.',
+            img: img3
+        },
+    ];
+
+    
     return (
         <div>
             <div>
@@ -38,9 +51,9 @@ const Services = () => {
             <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 justify-items-center gap-3 px-5'>
                 {
                     services.map((service) =>
-                        <div className='grid justify-items-start  hover:scale-125 transition duration-300 ease-in-out'>
+                        <div className='grid justify-items-start  hover:scale-125 transition duration-500 ease-in-out'>
                             <figure className="px-10 pt-10">
-                                <img src="https://placeimg.com/400/225/arch" alt="Shoes" className="rounded-xl" />
+                                <img src={service.img} alt="Shoes" className="rounded-xl" />
                             </figure>
                             <div className="card-body items-center text-center">
                                 <h2 className="card-title">{service.name}</h2>
