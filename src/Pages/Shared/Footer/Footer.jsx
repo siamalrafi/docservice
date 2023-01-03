4b164982c3f68037a6da8c1fc5127fa93e8d2101
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-03 21:18:44 +0600

    Thsi si thefooter

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -15,30 +15,24 @@ const Footer = () => {
                 className=" p-10">
                 <div className='footer'>
                     <div>
-                        <span className="footer-title">Services</span>
+                        <span className="text-gray-700 text-1xl font-extrabold">Services</span>
                         <Link className="link link-hover">Branding</Link>
                         <Link className="link link-hover">Design</Link>
                         <Link className="link link-hover">Marketing</Link>
                         <Link className="link link-hover">Advertisement</Link>
                     </div>
                     <div>
-                        <span className="footer-title">Company</span>
+                        <span className=" text-gray-700 text-1xl font-extrabold">Company</span>
                         <Link className="link link-hover">About us</Link>
                         <Link className="link link-hover">Contact</Link>
                         <Link className="link link-hover">Jobs</Link>
                         <Link className="link link-hover">Press kit</Link>
                     </div>
-                    <div>
-                        <span className="footer-title">Legal</span>
-                        <Link className="link link-hover">Terms of use</Link>
-                        <Link className="link link-hover">Privacy policy</Link>
-                        <Link className="link link-hover">Cookie policy</Link>
-                    </div>
                     <div className="text-center ">
                         <h3 className="text-2xl font-bold mb-2 "> Download our app </h3>
                         <p className='text-2xl'> Stay fit. All day, every day. </p>
                         <div className="flex justify-center my-5">
-                            <div className="bg-gray-700 flex items-center border rounded-lg px-4 py-2 w-52 mx-2">
+                            <div className="bg-gray-700 flex items-center border rounded-lg px-4 py-2 mx-2">
                                 <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" alt='google play' className="w-7 md:w-8" />
 
                                 <div className="text-left ml-3">
@@ -46,9 +40,8 @@ const Footer = () => {
                                     <p className="  text-white"> Google Play Store </p>
                                 </div>
                             </div>
-                            <div className="flex bg-gray-700 items-center border rounded-lg px-4 py-2 w-44 mx-2">
+                            <div className="flex bg-gray-700 items-center border rounded-lg px-4 py-2 mx-2">
                                 <FontAwesomeIcon className='text-4xl text-white' icon={faAppleWhole}></FontAwesomeIcon>
-
                                 <div className="text-left ml-3">
                                     <p className="text-white">Download on </p>
                                     <p className="text-white"> Apple Store </p>
@@ -56,8 +49,8 @@ const Footer = () => {
                             </div>
                         </div>
                     </div>
-
                 </div>
+
                 <div className='mt-16 text-center'>
                     <p>Copyright © 2022 - All right reserved by RHD Industries Ltd</p>
                 </div>
