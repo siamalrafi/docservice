8eb2e82187b1024f14d2e095df93ca2135517b16
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-06 15:15:12 +0600

    This is the next

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,11 +2,11 @@ import Home from "../Pages/Home/Home";
 import SignUp from '../Pages/SignUp/SignUp';
 import Login from '../Pages/Login/Login';
 import About from '../Pages/About/About';
-import Services from "../Pages/Services/Services";
+// import Services from "../Pages/Services/Services";
 import Blog from '../Pages/Blog/Blog';
 import Dashboard from '../Pages/Dashboard/Dashboard';
 import PrivateRoutes from './PrivateRoutes';
-
+import Services from '../Pages/Home/Services/Services';
 
 const { createBrowserRouter } = require("react-router-dom");
 const { default: Main } = require("../Layout/Main/Main");
