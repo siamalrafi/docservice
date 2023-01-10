da26eb01c923f4c9161237f94a48990d394a331a
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-10 18:13:05 +0600

    This is ht enext

--- src/Pages/Services/AvailableAppointments/AppointmentOption.jsx
+++ src/Pages/Services/AvailableAppointments/AppointmentOption.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const AppointmentOption = () => {
+const AppointmentOption = ({ appointmentOption, setTreatment }) => {
+    const { name, slots } = appointmentOption;
     return (
-        <div>
-            <h1>AppointmentOption</h1>
-            <h1>AppointmentOption</h1>
-            <h1>AppointmentOption</h1>
+        <div className="card shadow-xl">
+            <div className="card-body text-center">
+                <h2 className="text-2xl text-secondary font-bold text-center">{name}</h2>
+                <p>{slots.length > 0 ? slots[0] : 'Try Another day'}</p>
+                <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
+                <div className="card-actions justify-center">
+                    <label
+                        disabled={slots.length === 0}
+                        htmlFor="booking-modal"
+                        className="btn btn-primary text-white"
+                        onClick={() => setTreatment(appointmentOption)}
+                    >Book Appointment</label>
+                </div>
+            </div>
         </div>
     );
 };
