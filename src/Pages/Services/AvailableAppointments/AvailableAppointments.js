f79e9c15e813d37a37e8daf173dee75f06b27669
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-10 18:08:39 +0600

    Thsi is hte next

--- src/Pages/Services/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Services/AvailableAppointments/AvailableAppointments.js
@@ -1,13 +1,40 @@
+import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppointmentOption from '../AvailableAppointments/AppointmentOption'
 
-const AvailableAppointments = ({selectedDate}) => {
+
+const AvailableAppointments = ({ selectedDate }) => {
+    // const [appointmentOptions, setAppointmentOptions] = useState([]);
+    const [treatment, setTreatment] = useState(null);
+
+
+    const { data: appointmentOptions = [], isLoading, refetch } = useQuery({
+        queryKey: ['appointmentOptions'],
+        queryFn: async () => {
+            const res = await fetch(`/appointmentOptions.json`)
+            const data = await res.json();
+            return data
+        }
+    });
+
+    console.log(appointmentOptions);
+
+
     return (
         <div>
-            <h1>AvailableAppointments
-                AvailableAppointments</h1>
             <section className='my-10'>
                 <p className='text-center text-secondary font-bold'>Available Appointments on {format(selectedDate, 'PP')}</p>
+
+                <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                    {
+                        appointmentOptions.map(option => <AppointmentOption
+                            key={option._id}
+                            appointmentOption={option}
+                            setTreatment={setTreatment}
+                        ></AppointmentOption>)
+                    }
+                </div>
             </section >
         </div>
     );
