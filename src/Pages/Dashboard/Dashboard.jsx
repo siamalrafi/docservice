8c4b2c42f530ee05dbb96333cfe670b58d56118e
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-20 17:41:34 +0600

    This is the drawer :

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div>
-            <h1>DashboardDashboardDashboard</h1>
+            <div className="drawer drawer-mobile">
+                <input id="deshboard-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-side">
+                    <label htmlFor="deshboard-drawer" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+                    </ul>
+
+                </div>
+            </div>
         </div>
     );
 };
