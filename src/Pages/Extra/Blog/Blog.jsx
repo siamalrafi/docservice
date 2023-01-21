db0aaf0c70b252e2046a903da40475d7037fad9c
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-22 00:16:52 +0600

    dkfj

--- src/Pages/Extra/Blog/Blog.jsx
+++ src/Pages/Extra/Blog/Blog.jsx
@@ -3,7 +3,66 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h1>BlogBlogBlog</h1>
+            <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-5">
+                <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+                    <div>
+                        <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+                            Make history
+                        </p>
+                    </div>
+                    <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+                        <span className="relative inline-block">
+                            <svg
+                                viewBox="0 0 52 24"
+                                fill="currentColor"
+                                className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                            >
+                                <defs>
+                                    <pattern
+                                        id="84d09fa9-a544-44bd-88b2-08fdf4cddd38"
+                                        x="0"
+                                        y="0"
+                                        width=".135"
+                                        height=".30"
+                                    >
+                                        <circle cx="1" cy="1" r=".7" />
+                                    </pattern>
+                                </defs>
+                                <rect
+                                    fill="url(#84d09fa9-a544-44bd-88b2-08fdf4cddd38)"
+                                    width="52"
+                                    height="24"
+                                />
+                            </svg>
+                            <span className="relative">Let's</span>
+                        </span>{' '}
+                        Learn .....
+                    </h2>
+                    <p className="text-base text-gray-700 md:text-md">
+                        Consulting a healthcare professional, buying medicine, ordering lab tests and improving your health and wellbeing, all can be done 24/7 with DocTime!
+                    </p>
+                </div>
+
+
+                {/* Out all Blog is Here. */}
+
+
+                <div className="grid gap-8 row-gap-5 md:row-gap-8 lg:grid-cols-3">
+                    <div className="p-5 duration-300 transform bg-white border-2 border-purple-400 rounded shadow-sm border-deep-purple-accent-100 hover:-translate-y-2">
+                        <div className="flex items-center mb-2">
+                            <p className="flex items-center justify-center w-10 h-10 mr-2 text-lg font-bold text-white rounded-full bg-deep-purple-accent-400">
+                                1
+                            </p>
+                            <p className="text-lg font-bold leading-5">Fill her up</p>
+                        </div>
+                        <p className="text-sm text-gray-900">
+                            Bro ipsum dolor sit amet gaper backside single track, manny Bike
+                            epic clipless.
+                        </p>
+                    </div>
+                 
+                </div>
+            </div>
         </div>
     );
 };
