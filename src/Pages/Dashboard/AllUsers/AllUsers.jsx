56730517b4ffbc10c9ad314cd19ccbee911608f8
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-21 23:05:15 +0600

    This is the delete

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,9 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
+import ConfirmationModal from '../../Shared/ConfirmationModal/ConfirmationModal';
+import Loading from '../../Shared/LoadingPage/LoadingPage';
 
 const AllUsers = () => {
-    const { data: users = [], refetch } = useQuery({
+    const [deletingUser, setDeletingUser] = useState(null);
+
+    const closeModal = () => {
+        setDeletingUser(null);
+    }
+
+
+    const { data: users = [], isLoading, refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/users');
@@ -29,7 +38,30 @@ const AllUsers = () => {
                     refetch();
                 }
             })
+    };
+
+
+
+    const handleDeleteBooking = user => {
+        fetch(`http://localhost:5000/users/${user._id}`, {
+            method: 'DELETE',
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    refetch();
+                    toast.success(`User deleted successfully`)
+                }
+            });
     }
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
 
 
     return (
@@ -53,13 +85,29 @@ const AllUsers = () => {
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
                                 <td>{user?.role !== 'admin' && <button onClick={() => handleMakeAdmin(user._id)} className='btn btn-xs btn-primary'>Make Admin</button>}</td>
-                                <td><button className='btn btn-xs btn-danger'>Delete</button></td>
+                                <td>
+                                    <label onClick={() => setDeletingUser(user)} htmlFor="confirmation-modal" className="text-center btn btn-circle btn-outline btn-sm">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                    </label>
+                                </td>
                             </tr>)
                         }
 
                     </tbody>
                 </table>
             </div>
+
+            {
+                deletingUser && <ConfirmationModal
+                    title={`Are you sure you want to delete?`}
+                    message={`If you delete. It cannot be undone.`}
+                    successAction={handleDeleteBooking}
+                    successButtonName="Delete"
+                    modalData={deletingUser}
+                    closeModal={closeModal}
+                >
+                </ConfirmationModal>
+            }
         </div>
     );
 };
