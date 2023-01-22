1b08d30b4dddc7b3e7a105c19dd4ea79fd91b0b1
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-22 23:18:56 +0600

    This is the next

--- src/Delete.jsx
+++ src/Delete.jsx
@@ -7,7 +7,7 @@ import { AuthContext } from '../../../Context/UserContext';
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
 
-    const manuItems = <React.Fragment>
+    const manuItems= <React.Fragment>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
         <li><Link to='/services'>Services</Link></li>
@@ -63,32 +63,26 @@ const Navbar = () => {
     return (
         <div>
             <div className="navbar font-semibold bg-gradient-to-r from-indigo-400 via-purple-300 to-pink-500 flex justify-between">
-                <div className="flex-1">
-                  
-                    {/* Dashboard Icon */}
-                    <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
-                        <FontAwesomeIcon title='OpenDashboard' className='text-3xl' icon={faDashboard}></FontAwesomeIcon>
-                    </label>
-                    <a href='/' className="btn btn-ghost text-xl font-bold uppercase">DocService</a>
-                </div>
-
-                <div className="flex-none">
-                    <ul className=" navbar-center hidden lg:flex menu menu-horizontal p-0">
-                        {manuItems}
-                    </ul>
-
-                    <div className="dropdown dropdown-end btn-ghost lg:hidden">
-                        <button className="btn btn-square btn-ghost">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                        </button>
-                        <ul tabIndex={0} className=" bg-base-100 dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             {manuItems}
                         </ul>
                     </div>
+                    <Link to='/' className="btn btn-ghost text-xl normal-case">DocService</Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
+                        {manuItems}
+                    </ul>
                 </div>
 
-
-
+                <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
+                    <FontAwesomeIcon title='OpenDashboard' className='text-3xl' icon={faDashboard}></FontAwesomeIcon>
+                </label>
 
 
             </div>
